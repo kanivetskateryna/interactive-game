e2bc5d3767944375aafad576661b2218ca4efa89
Kateryna Kanivets <kateryna.kanivets@lseg.com>
2024-05-06 18:56:57 +0200

    added java doc for strategy, tree node and visitor configs

--- src/main/java/config/TreeNodeConfig.java
+++ src/main/java/config/TreeNodeConfig.java
@@ -10,6 +10,10 @@ import org.springframework.context.annotation.Import;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * This class provides configuration for the tree nodes used in the game scenario.
+ * It imports the ScenarioTextConfig class and uses the text nodes defined there to create the tree nodes.
+ */
 @Configuration
 @Import({ScenarioTextConfig.class})
 public class TreeNodeConfig {
@@ -17,26 +21,56 @@ public class TreeNodeConfig {
     @Autowired
     private ScenarioTextConfig scenario;
 
+    /**
+     * Provides a bean of type TreeNode that represents the end node of the game.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode endNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.endingText(), List.of(), List.of());
     }
 
+    /**
+     * Provides a bean of type TreeNode that represents the node where the player gets dressed.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode getDressedNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.pickingThingsUpWithoutFirstAidKitText(), List.of(), List.of());
     }
 
+    /**
+     * Provides a bean of type TreeNode that represents the node where the player picks things up.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode pickThingsUpNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.pickingThingsUpWithFirstAidKitText(), List.of(), List.of());
     }
 
+    /**
+     * Provides a bean of type TreeNode that represents the node where Clara is not calmed down.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode notCalmedDownNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.claraStillWorriedText(), List.of(), List.of());
     }
 
+    /**
+     * Provides a bean of type TreeNode that represents the node where Clara is worried.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode claraNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.worriedClaraText(),
@@ -45,16 +79,34 @@ public class TreeNodeConfig {
                 List.of(notCalmedDownNode(), notCalmedDownNode()));
     }
 
+    /**
+     * Provides a bean of type TreeNode that represents the node where the team meets death.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode teamMeetsDeathNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.losingText(), List.of(), List.of());
     }
 
+    /**
+     * Provides a bean of type TreeNode that represents the node where the winning option is presented.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode winningOptionNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.winningText(), List.of(), List.of());
     }
 
+    /**
+     * Provides a bean of type TreeNode that represents the node where the player has a first aid kit.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode firstAidKitWithYouNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.usefulFirstAidKitText(),
@@ -63,6 +115,12 @@ public class TreeNodeConfig {
                 List.of(winningOptionNode(), teamMeetsDeathNode()));
     }
 
+    /**
+     * Provides a bean of type TreeNode that represents the node where the player finds a walkie talkie.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode walkieTalkieNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.goingToGarageToHearSecretVirusNewsText(),
@@ -72,6 +130,12 @@ public class TreeNodeConfig {
                 List.of(endNode(), getDressedNode(), pickThingsUpNode()));
     }
 
+    /**
+     * Provides a bean of type TreeNode that represents the node where the player watches TV.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode watchTvNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.goingToHouseToSeeSecretVirusNewsText(),
@@ -81,6 +145,12 @@ public class TreeNodeConfig {
                 List.of(endNode(), getDressedNode(), pickThingsUpNode()));
     }
 
+    /**
+     * Provides a bean of type TreeNode that represents the node where the player encounters a helicopter.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode helicopterNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.oneMoreHelicopterText(),
@@ -89,6 +159,12 @@ public class TreeNodeConfig {
                 List.of(walkieTalkieNode(), endNode()));
     }
 
+    /**
+     * Provides a bean of type TreeNode that represents the node where the player encounters news.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode newsNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.urgentNewsText(),
@@ -97,6 +173,12 @@ public class TreeNodeConfig {
                 List.of(watchTvNode(), endNode()));
     }
 
+    /**
+     * Provides a bean of type TreeNode that represents the beginning node of the game.
+     *
+     * @return a new TreeNode
+     * @throws IOException if an I/O error occurs
+     */
     @Bean
     public TreeNode beginningNode() throws IOException {
         return NodeFactory.createNodeWithChoices(scenario.beginningText(),
--- src/main/java/config/VisitorConfig.java
+++ src/main/java/config/VisitorConfig.java
@@ -5,11 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import visitor.NodeVisitor;
 import visitor.OutputNodeVisitor;
 
+/**
+ * Configuration class for visitor components.
+ * This class defines the beans related to node visitors in the application.
+ */
 @Configuration
 public class VisitorConfig {
 
+    /**
+     * Creates and returns a {@link NodeVisitor} bean.
+     * The {@link NodeVisitor} provided by this method is an instance of {@link OutputNodeVisitor},
+     * which likely performs some specific formatting or processing of nodes.
+     *
+     * @return the {@link NodeVisitor} that handles the output processing of nodes
+     */
     @Bean
     public NodeVisitor visitor() {
         return new OutputNodeVisitor();
     }
 }
+
